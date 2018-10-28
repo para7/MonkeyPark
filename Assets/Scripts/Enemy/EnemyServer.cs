e5b7288af55e0bd879a570244c9c9ac05a54c03f
para7 <minstrel.suppin@gmail.com>
2018-10-28 13:45:03 +0900

    progress

--- Assets/Scripts/Enemy/EnemyServer.cs
+++ Assets/Scripts/Enemy/EnemyServer.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Park.Enemy
 {
     public class EnemyServer : MonoBehaviour
     {
+        private Vector3[] spawns;
+
         // Use this for initialization
         void Start()
         {
-
+            spawns = GetComponentsInChildren<Spawn>().Select(x => x.transform.position).ToArray();
         }
 
         // Update is called once per frame
