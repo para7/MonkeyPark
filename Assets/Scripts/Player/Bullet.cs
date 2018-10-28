b25948b9030878446007c8801163f8106ae2a797
para7 <minstrel.suppin@gmail.com>
2018-10-28 13:31:37 +0900

    progress

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Park.Player
+{
+    public class Bullet : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        public float speed = 50f;
+        public float lifetime = 0.3f;
+        private float time = 0;
+        public float fallSpeed = -0.3f;
+        public float shake = 0.1f;
+        public float dashShake = 0.8f;
+
+        private Vector3 randomSpd;
+
+        private void Awake()
+        {
+            speed += Random.Range(-speed / 20f, speed / 20f);
+
+            //ブレ変数
+            float random = 0;
+
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.01f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.01f)
+            {
+                random = Input.GetKey(KeyCode.LeftShift) ? dashShake : shake;
+            }
+
+            randomSpd = new Vector3(Random.Range(-random, random), Random.Range(-random, random), Random.Range(-random, random));
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            this.transform.position += this.transform.forward * Time.deltaTime * speed + randomSpd;
+
+            time += Time.deltaTime;
+
+            if (time > lifetime / 2)
+            {
+                speed *= 0.95f;
+                transform.position += new Vector3(0, fallSpeed, 0);
+            }
+
+            if (time > lifetime)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
--- Assets/Sprites/LookCamera.cs
+++ Assets/Sprites/LookCamera.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookCamera : MonoBehaviour
+namespace Park.Enemy
 {
-    public bool enableJumpRotate = true;
-
-    // Use this for initialization
-    void Start()
+    public class LookCamera : MonoBehaviour
     {
+        public bool enableJumpRotate = true;
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //https://www.f-sp.com/entry/2017/08/30/171353
-
-        // メインカメラの取得
-        if (Camera.main) // カメラ有効時のみ
+        // Use this for initialization
+        void Start()
         {
-            // カメラに向かう方向を計算
-            Vector3 forward = DelayCameraPos.GetDelayCameraPos() - transform.position;
 
-            if (!enableJumpRotate)
-            {
-                forward = new Vector3(-forward.x, 0, -forward.z);
-            }
+        }
 
-            if (forward != Vector3.zero) // 零ベクトルでない
+        // Update is called once per frame
+        void Update()
+        {
+            //https://www.f-sp.com/entry/2017/08/30/171353
+
+            // メインカメラの取得
+            if (Camera.main) // カメラ有効時のみ
             {
-                // カメラの向きを正面とする回転を作成して適用
-                transform.rotation = Quaternion.LookRotation(forward);
+                // カメラに向かう方向を計算
+                Vector3 forward = Park.Player.DelayCameraPos.GetDelayCameraPos() - transform.position;
+
+                if (!enableJumpRotate)
+                {
+                    forward = new Vector3(-forward.x, 0, -forward.z);
+                }
+
+                if (forward != Vector3.zero) // 零ベクトルでない
+                {
+                    // カメラの向きを正面とする回転を作成して適用
+                    transform.rotation = Quaternion.LookRotation(forward);
+                }
             }
         }
     }
